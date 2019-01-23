aecf5d8bc0ae5dd05885d565ecef3f5a2ef95b4d
lj7788 <lj7788@126.com>
2019-01-23 22:46:08 +0800

    Init

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # vddlplus
+vddl --> (https://github.com/hejianxian/vddl)
+
+vddl 增强版本，修改了拖拽算法,保留容器中原来的状态
+vddl 原来版本容器中的对象拖拽后会重新加载
 
 ## Project setup
 ```
@@ -27,3 +31,8 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+效果图如下:
+![拖拽前](https://github.com/lj7788/vddlplus/raw/master/public/001.png)
+
+![拖拽后](https://github.com/lj7788/vddlplus/raw/master/public/002.png)
---
+++ src/views/Demo1.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="demo1">
+  <vddl-list class="panel__body--list" :list="list2" :horizontal="false">
+                  <vddl-draggable class="panel__body--item no-padding-left" v-for="(item, index) in list2" :key="item.idx"
+                    :draggable="item"
+                    :index="index"
+                    :wrapper="list2"
+                    effect-allowed="move">
+                        <div>{{item.key}}</div>                      
+                        <input/>
+                  </vddl-draggable>
+              </vddl-list>
+    </div>
+</template>
+
+<script>
+import Vue from 'vue'
+  Vue.prototype.vddlDropEffectWorkaround = {};
+  Vue.prototype.vddlDragTypeWorkaround = {};
+import vddlList from '@/components/vddl/vddl-list'
+import vddlDraggable from '@/components/vddl/vddl-draggable'
+export default {
+    components:{
+        vddlList,
+        vddlDraggable
+    },
+    data(){
+        return {
+            list:[{idx:0,key:'A1'},{idx:1,key:'A2'},{idx:2,key:'A3'},{idx:3,key:'A4'}]
+        }
+    },
+    computed:{
+        list2(){
+            let data=this.list
+            return data.sort((a,b)=>a.idx>b.idx)
+        }
+    }
+}
+</script>
+
+<style>
+    .demo1{
+        margin: 10px 20px;
+    }
+    .demo1 .panel__body--item{
+        border-radius: 8px;
+        border: solid 1px #ccc;
+        min-height: 100px;
+        margin-top: 20px;
+        box-shadow: rgba(0, 0, 0, 0.12) 0px 2px 6px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+    }
+    .demo1 .vddl-placeholder {
+        width: 100%;
+        background-color: rgba(0, 0, 0, 0.12);
+        border-radius: 8px;
+        border: solid 1px #ccc;
+        min-height: 100px;
+        margin-top: 20px;
+        box-shadow: rgba(0, 0, 0, 0.12) 0px 2px 6px, rgba(0, 0, 0, 0.24) 0px 1px 2px;
+    }
+    .demo1 .vddl-dragging.vddl-dragging-source {
+        display: none;
+    }
+</style>
